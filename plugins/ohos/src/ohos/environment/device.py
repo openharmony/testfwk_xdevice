049638648acef7f45b8a66a8583d07d296466d26
deveco_xdevice <liguangjie1@huawei.com>
2022-12-08 15:43:48 +0800

    修复设备异常时还分配测试套的bug
    Signed-off-by: deveco_xdevice <liguangjie1@huawei.com>

--- plugins/ohos/src/ohos/environment/device.py
+++ plugins/ohos/src/ohos/environment/device.py
@@ -32,6 +32,7 @@ from xdevice import exec_cmd
 from xdevice import ConfigConst
 from xdevice import HdcError
 from xdevice import DeviceAllocationState
+from xdevice import TestDeviceState
 from xdevice import convert_serial
 from xdevice import check_path_legal
 from xdevice import start_standing_subprocess
@@ -542,6 +543,9 @@ class Device(IDevice):
     def set_recover_state(self, state):
         with self.device_lock:
             setattr(self, ConfigConst.recover_state, state)
+            if not state:
+                self.test_device_state = TestDeviceState.NOT_AVAILABLE
+                self.device_allocation_state = DeviceAllocationState.unavailable
 
     def get_recover_state(self, default_state=True):
         with self.device_lock:
