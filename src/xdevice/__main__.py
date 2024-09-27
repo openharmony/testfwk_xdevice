89352ddbe12967329011d3d8ad731fc6d9cba824
deveco_xdevice <liguangjie1@huawei.com>
2024-09-27 11:04:10 +0800

    修改报告模板下载到程序运行前
    Signed-off-by: deveco_xdevice <liguangjie1@huawei.com>

--- src/xdevice/__main__.py
+++ src/xdevice/__main__.py
@@ -17,6 +17,7 @@
 #
 import os
 import sys
+from urllib import request
 from xdevice import Console
 from xdevice import platform_logger
 from xdevice import VERSION
@@ -57,12 +58,58 @@ def check_report_template():
         if not os.path.exists(tmp_file):
             missing_files.append(tmp_file)
     if len(missing_files) > 0:
+        return False
+    return True
+
+
+def download_template_resource():
+    LOG.info("Start download report template...")
+    sources = [
+        {
+            "file": "static/css/element-plus@2.3.4_index.css",
+            "url": "https://cdn.bootcdn.net/ajax/libs/element-plus/2.3.4/index.css"
+        },
+        {
+            "file": "static/element-plus@2.3.4_index.full.js",
+            "url": "https://cdn.bootcdn.net/ajax/libs/element-plus/2.3.4/index.full.js"
+        },
+        {
+            "file": "static/element-plus_icons-vue@2.0.10_index.iife.min.js",
+            "url": "https://cdn.bootcdn.net/ajax/libs/element-plus-icons-vue/2.0.10/index.iife.min.js"
+        },
+        {
+            "file": "static/EventEmitter.js",
+            "url": "https://cdn.bootcdn.net/ajax/libs/EventEmitter/5.2.8/EventEmitter.js"
+        },
+        {
+            "file": "static/vue@3.2.41_global.min.js",
+            "url": "https://cdn.bootcdn.net/ajax/libs/vue/3.2.41/vue.global.min.js"
+        }
+    ]
+    resource_path = os.path.join(os.path.dirname(__file__), "_core", "resource")
+    template_path = os.path.join(resource_path, "template")
+    for source in sources:
+        file, url = source.get("file"), source.get("url")
+        to_path = os.path.join(template_path, file)
+        if os.path.exists(to_path):
+            continue
+        LOG.info(f"get report template resource {file} from {url}")
+        try:
+            response = request.urlopen(url, timeout=5)
+            with open(to_path, 'wb') as f:
+                f.write(response.read())
+        except Exception as e:
+            LOG.error(f"get report template resource error, {e}")
+    LOG.info("Finish download report template...")
+    # check report template
+    if not check_report_template():
         LOG.error("------" * 5)
         LOG.error(notice_zh.format(resource_path=resource_path))
         LOG.error(notice_en.format(resource_path=resource_path))
         LOG.error("------" * 5)
         return False
-    return True
+    else:
+        return True
 
 
 def main_process(command=None):
@@ -70,7 +117,8 @@ def main_process(command=None):
         "*************** xDevice Test Framework %s Starting ***************" %
         VERSION)
     if not check_report_template():
-        return
+        if not download_template_resource():
+            return
     if command:
         args = str(command).split(" ")
         args.insert(0, "xDevice")
