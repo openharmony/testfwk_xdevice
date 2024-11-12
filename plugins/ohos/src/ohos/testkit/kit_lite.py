92becce10d711e77e9af5a62cb0de389df3eb610
deveco_xdevice <liguangjie1@huawei.com>
2024-11-12 17:54:26 +0800

    1、修复L1 mount异常的问题；2、优化L1 querySmall.bin的运行逻辑
    Signed-off-by: deveco_xdevice <liguangjie1@huawei.com>

--- plugins/ohos/src/ohos/testkit/kit_lite.py
+++ plugins/ohos/src/ohos/testkit/kit_lite.py
@@ -58,6 +58,9 @@ def execute_query(device, query):
     if not query:
         LOG.debug("query bin is none")
         return
+    if device.device_props:
+        LOG.debug("query bin has been executed")
+        return
     LOG.debug("execute query bin begins")
     if device.__get_device_kernel__() == DeviceLiteKernel.linux_kernel:
         command = f"chmod +x /storage{query} && /storage{query}"
@@ -239,14 +242,17 @@ class MountKit(ITestKit):
             if target in self.mounted_dir:
                 LOG.debug("%s is mounted" % target)
                 continue
+            if target == "/test_root/tools" and device.device_props:
+                LOG.debug("query bin has been executed, '/test_root/tools' no need to mount again")
+                continue
             mkdir_on_board(device, target)
 
-            # local nfs server need use alias of dir to mount
+            temp_linux_directory = linux_directory
             if is_remote.lower() == "false":
-                linux_directory = get_mount_dir(linux_directory)
+                temp_linux_directory = get_mount_dir(linux_directory)
             for command in commands:
                 command = command.replace("nfs_ip", linux_host). \
-                    replace("nfs_directory", linux_directory).replace(
+                    replace("nfs_directory", temp_linux_directory).replace(
                     "device_directory", target).replace("//", "/")
                 timeout = 15 if command.startswith("mount") else 1
                 if command.startswith("mount"):
